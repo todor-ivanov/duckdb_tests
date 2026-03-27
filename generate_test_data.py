405dfbce8d27cba2b3bc38ec30712ec40447e54c
Federico Stagni <federico.stagni@cern.ch>
2026-03-27 15:55:57 +0100

    f

--- generate_test_data.py
+++ generate_test_data.py
@@ -30,36 +30,52 @@ CREATE TABLE PilotAttributes (
 """)
 
 # Generate random data for the last days
-start_date = datetime.now() - timedelta(days=2)
+start_date = datetime.now() - timedelta(days=90)
 end_date = datetime.now()
 
 
-# Insert random data into 'Pilots' and 'PilotAttributes'
-def random_multiple_of_x(x=1, from_n=1, to_n=1000):
+def random_multiple_of_x(x=1, from_n=1, to_n=1000, weights=None):
+    """helper to generate random data into 'Pilots' and 'PilotAttributes'"""
     # Calculate the number of steps between from_n and to_n
     steps = (to_n - from_n) // x + 1
-    # Generate a random step and multiply by x
-    return random.randint(1, steps) * x
+
+    # Generate a list of all possible multiples
+    multiples = [from_n + i * x for i in range(steps)]
+    # Use random.choices if weights are provided, otherwise use random.choice
+    if weights is not None:
+        return random.choices(multiples, weights=weights, k=1)[0]
+    else:
+        return random.choices(multiples)
 
 
 statuses = ["Running", "Waiting", "Done", "Failed"]
+status_weights = [0.5, 0.1, 0.39, 0.01]
+cores = [1, 8, 16, 256]
+core_weights = [0.6, 0.1, 0.25, 0.05]
+memory = [1024, 2048, 4096, 8092]
+memory_weights = [0.1, 0.3, 0.3, 0.2]
 
-for pilot_id in range(1, 1_000_001):
-    update_time = start_date + timedelta(
+NUM_PILOTS = 100_000
+
+update_times = [
+    start_date + timedelta(
         days=random.randint(0, 1),
         hours=random.randint(0, 24),
         minutes=random.randint(0, 60),
         seconds=random.randint(0, 60),
     )
-    status = random.choice(statuses)
-    conn.execute(
-        "INSERT INTO Pilots (UpdateTime, Status) VALUES (?, ?)", (update_time, status)
-    )
+    for _ in range(NUM_PILOTS)
+]
+status_list = random.choices(statuses, weights=status_weights, k=NUM_PILOTS)
+cores_list = random.choices(cores, weights=core_weights, k=NUM_PILOTS)
+memory_list = random.choices(memory, weights=memory_weights, k=NUM_PILOTS)
 
-    cores = random_multiple_of_x(8, 1, 16)
-    memory_mb = random_multiple_of_x(1024, 1024, 32768)
-    conn.execute(
-        "INSERT INTO PilotAttributes (PilotID, Cores, MemoryMB) VALUES (?, ?, ?)",
-        (pilot_id, cores, memory_mb),
-    )
+conn.executemany(
+    "INSERT INTO Pilots (UpdateTime, Status) VALUES (?, ?)",
+    zip(update_times, status_list)
+)
+conn.executemany(
+    "INSERT INTO PilotAttributes (PilotID, Cores, MemoryMB) VALUES (?, ?, ?)",
+    zip(range(1, NUM_PILOTS + 1), cores_list, memory_list)
+)
 conn.execute("EXPORT DATABASE 'pilots' (FORMAT parquet);")
